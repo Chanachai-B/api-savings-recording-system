6ca14d42de792aa861ce7ae351272833cd0e3254
Nui <baisaenc@gmail.com>
2025-11-09 18:44:50 +0700

    feat [api] : add redirect api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.api.routes import transaction_routes
 
 app = FastAPI(title="School Savings API", version="1.0")
 
 app.include_router(transaction_routes.router, prefix="/transactions", tags=["Transactions"])
 
-@app.get("/")
-def root():
-    return {"message": "School Savings API is running 🚀"}
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
